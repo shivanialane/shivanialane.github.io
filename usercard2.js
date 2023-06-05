187b0e6bbfdb8145bdd3ca0e06f44226aebdaf48
shivanialane <93445839+shivanialane@users.noreply.github.com>
2023-06-05 22:39:03 +0530

    js changed

--- usercard2.js
+++ usercard2.js
@@ -5,5 +5,22 @@ let viratoldinfo={
 
 }
 let viratyounginfo={
-    
+    imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUxN38RTTHyiY42ieVunge1SzUcfg4ZBGRDLgqK2HBMA&usqp=CAU&ec=48600112",
+    name:"King kohli",
+    Description:"Daring, tough and fiercely talented"
+}
+let isviratold=true;
+let displayobject;
+let flipdata=function(){
+    if(isviratold==true){
+        displayobject=viratyounginfo;
+        isviratold=false;
+    }else{
+        displayobject=viratoldinfo;
+        isviratold=true;
+    }
+    document.getElementById("virat-img").src=displayobject.imgurl;
+    document.getElementById("virat-name").innerHTML=displayobject.name;
+    document.getElementById("virat-description").innerHTML=displayobject.Description;
+
 }
