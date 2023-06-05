2dd4c098ebff3bc721ed6b36643cdda24891e46a
shivanialane <93445839+shivanialane@users.noreply.github.com>
2023-06-05 22:22:44 +0530

    cha

---
+++ usercard2.js
@@ -0,0 +1,9 @@
+let viratoldinfo={
+    imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNkp-mTpgcYxZVf1DPztA2fkE-HGiJMtY5QvqRcjP4Ow&usqp=CAU&ec=48600112",
+    name:"virat",
+    Description:"Virat Kohli is an Indian cricketer who plays for India national cricket team"
+
+}
+let viratyounginfo={
+    
+}
