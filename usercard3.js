969f0ef52157f3a7720b9ca3f28561eab26413af
shivanialane <93445839+shivanialane@users.noreply.github.com>
2023-06-13 22:19:28 +0530

    done changes

--- usercard3.js
+++ usercard3.js
@@ -9,9 +9,13 @@ let displayobject;
 let getRandomUser=function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
-    .then(data => console.log(data))
-    document.getElementById("virat-img").src=displayobject.imgurl;
-    document.getElementById("virat-name").innerHTML=displayobject.name;
-    document.getElementById("virat-description").innerHTML=displayobject.Description;
-
+    .then(data=>{
+        displayobject.name=data.results[0].name.first +" "+ data.results[0].name.last
+        displayobject.imgurl=data.results[0].picture.large
+        displayobject.Description=data.results[0].gender
+        document.getElementById("virat-img").src=displayobject.imgurl;
+        document.getElementById("virat-name").innerHTML=displayobject.name;
+        document.getElementById("virat-description").innerHTML=displayobject.Description;
+     })
+    
 }
